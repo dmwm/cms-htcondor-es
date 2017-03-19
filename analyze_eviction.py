1b6bd8b4a1fac26780a4f26d7f7088c1daa386f2
Brian Bockelman <bbockelm@cse.unl.edu>
2017-03-18 20:58:17 -0500

    Add simple tools for analyzing popularity data.

---
+++ analyze_eviction.py
@@ -0,0 +1,52 @@
+import sys
+import sqlite3
+
+conn = sqlite3.connect("popdb.sqlite")
+curs = conn.cursor()
+
+tier = "/%s" % sys.argv[1]
+eviction_months = 3
+
+datasets = {}
+for row in curs.execute("select dataset, size_bytes, events from dataset_size"):
+    datasets[row[0]] = {"size": row[1], "unique_events": row[2]}
+
+for row in curs.execute("select core_hours, events, read_bytes, dataset, crab_task, month from dataset_popularity"):
+    if row[3].endswith("/USER"): continue
+    if not row[3].endswith(tier): continue
+    info = datasets[row[3]]
+    core_hours = info.setdefault("core_hours", 0)
+    events = info.setdefault("events", 0)
+    read_bytes = info.setdefault("read_bytes", 0)
+    info.setdefault("crab_tasks", set()).add(row[4])
+    info['core_hours'] = core_hours + row[0]
+    info['events'] = events + row[1]
+    info['read_bytes'] = read_bytes + row[2]
+    monthly = info.setdefault("monthly", {201609:False, 201610:False, 201611:False, 201612:False, 201701:False})
+    monthly[row[5]] = True
+
+months = [201609, 201610, 201611, 201612, 201701]
+eviction_bytes = 0
+evicted_datasets = 0
+fetch_bytes = 0
+fetched_datasets = 0
+for dataset, info in datasets.items():
+    if 'monthly' not in info: continue
+    unused_count = 0
+    first_use = False
+    for month in months:
+        if not first_use:
+            if info['monthly'][month]:
+                first_use = True
+            continue
+        if not info['monthly'][month]:
+            unused_count += 1
+            if unused_count == eviction_months:
+                eviction_bytes += info['size']
+                evicted_datasets += 1
+        elif unused_count >= eviction_months:
+            fetch_bytes += info['size']
+            unused_count = 0
+            fetched_datasets += 1
+
+print "Eviction policy of %d months would have resulted in %.2f TB (%d datasets) evicted from disk and %.2fTB (%d datasets) fetched from tape." % (eviction_months, eviction_bytes/1e12, evicted_datasets, fetch_bytes/1e12, fetched_datasets)
---
+++ data_popularity_sizes.py
@@ -0,0 +1,53 @@
+
+import time
+import sqlite3
+import traceback
+import multiprocessing
+
+import dbs.apis.dbsClient
+
+global_url = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader'
+popdb = 'popdb.sqlite'
+
+api = None
+
+def fetch_summary(dataset):
+    global api
+    if not api:
+        api = dbs.apis.dbsClient.DbsApi(url=global_url)
+    try:
+        summary = api.listBlockSummaries(dataset=dataset[0])[0]
+        summary['dataset'] = dataset[0]
+    except:
+        traceback.print_exc()
+        raise
+    return summary
+
+def main():
+    conn = sqlite3.connect(popdb)
+    conn.isolation_level = None
+    curs = conn.cursor()
+
+    pool = multiprocessing.Pool(3)
+
+    iterable = [(row[0],) for row in curs.execute("select distinct(dp.dataset) from dataset_popularity as dp left outer join dataset_size as ds on (dp.dataset = ds.dataset) WHERE ds.size_bytes is null AND NOT (dp.dataset LIKE '%/USER')")]
+    print "There are %i datasets left to query." % len(iterable)
+
+    count = 0
+    curs.execute("BEGIN")
+    for info in pool.imap_unordered(fetch_summary, iterable):
+        print info
+        curs.execute("INSERT INTO dataset_size (dataset, size_bytes, events) VALUES (?, ?, ?)", (info['dataset'], info['file_size'], info['num_event']))
+        count += 1
+        if count % 50 == 0:
+            print time.ctime(), "Finished", count, "dataset records"
+            curs.execute("COMMIT")
+            curs.execute("BEGIN")
+    curs.execute("COMMIT")
+
+    pool.close()
+    pool.join()
+
+if __name__ == '__main__':
+    main()
+
