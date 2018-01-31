94452890994474679e986e8151dab40566e17c22
Brian Bockelman <bbockelm@cse.unl.edu>
2018-01-31 15:40:26 +0100

    Analyze usage based on real bytes used, not logical

--- analyze_usage.py
+++ analyze_usage.py
@@ -27,7 +27,7 @@ print "Start month: %d, Stop month: %d" % (start_month, stop_month)
 
 datasets = {}
 for row in curs.execute("select dataset, size_bytes, events, creation_date from dataset_size"):
-    datasets[row[0]] = {"size": row[1], "unique_events": row[2], "creation_date": row[3]}
+    datasets[row[0]] = {"size": row[1], "unique_events": row[2], "creation_date": row[3], 'replica_bytes': 0}
 
 replicas = collections.defaultdict(int)
 unique_tiers = set()
@@ -53,6 +53,10 @@ for row in curs.execute("SELECT core_hours, events, read_bytes, dataset, crab_ta
     info['events'] = events + row[1]
     info['read_bytes'] = read_bytes + row[2]
 
+for dataset, size_bytes in replicas.items():
+    info = datasets[dataset]
+    info['replica_bytes'] = size_bytes
+
 by_crab_tasks = {}
 total_bytes = 0
 total_unique_events = 0
@@ -70,13 +74,17 @@ for dataset, info in datasets.items():
     cinfo['events'] = events + info['events']
     unique_events = cinfo.setdefault('unique_events', 0)
     cinfo['unique_events'] = unique_events + info['unique_events']
+    replica_bytes = cinfo.setdefault('replica_bytes', 0)
+    cinfo['replica_bytes'] = replica_bytes + info['replica_bytes']
     total_bytes += info['size']
     total_unique_events += info['unique_events']
 
 unaccessed_bytes = 0
 unaccessed_old_bytes = 0
+tot_replica_bytes = 0
 for dataset, size_bytes in replicas.items():
     info = datasets[dataset]
+    tot_replica_bytes += size_bytes
     crab_task_count = len(info.get('crab_tasks', []))
     if not crab_task_count:
         if info['creation_date'] < old_cutoff:
@@ -84,23 +92,26 @@ for dataset, size_bytes in replicas.items():
         else:
             unaccessed_bytes += size_bytes
 
+
+print "Total replica bytes: %.1f" % (tot_replica_bytes/1e12)
 print "Number of CRAB3 tasks:"
 total_hours = 0
 for i in range(1, 15):
-    hours = by_crab_tasks.setdefault(i, {'events': 0, 'core_hours': 0, 'read_bytes': 0, 'bytes': 0})['core_hours']/1e6
+    hours = by_crab_tasks.setdefault(i, {'events': 0, 'core_hours': 0, 'read_bytes': 0, 'bytes': 0, 'replica_bytes': 0})['core_hours']/1e6
     total_hours += hours
-    print i, '%.1fTB in dataset' % (by_crab_tasks[i]['bytes']/1e12), ', %.2fM hours of analysis' % hours, '%.1fTB read' % (by_crab_tasks[i]['read_bytes']/1e12), '%.1fB events read' % (by_crab_tasks[i]['events']/1e9)
+    print i, '%.1fTB in dataset' % (by_crab_tasks[i]['replica_bytes']/1e12), ', %.2fM hours of analysis' % hours, '%.1fTB read' % (by_crab_tasks[i]['read_bytes']/1e12), '%.1fB events read' % (by_crab_tasks[i]['events']/1e9)
 
 overflow_bin = 15
 hours = (sum(val['core_hours'] for key, val in by_crab_tasks.items() if key > overflow_bin)/1e6)
 total_hours += hours
 read_bytes = (sum(val['read_bytes'] for key, val in by_crab_tasks.items() if key > overflow_bin)/1e12)
-tot_bytes = sum(val['bytes'] for key, val in by_crab_tasks.items() if key >= overflow_bin)/1e12
+#tot_bytes = sum(val['bytes'] for key, val in by_crab_tasks.items() if key >= overflow_bin)/1e12
+tot_bytes = sum(val['replica_bytes'] for key, val in by_crab_tasks.items() if key >= overflow_bin)/1e12
 events = (sum(val['events'] for key, val in by_crab_tasks.items() if key > overflow_bin)/1e9)
 unique_events = (sum(val['unique_events'] for key, val in by_crab_tasks.items() if key > overflow_bin)/1e9)
 print "overflow %.2fTB" % tot_bytes, "%.1fB unique events" % unique_events, '%.2fM hours' % hours, '%.1fTB read' % read_bytes, '%.1fB events read' % events
 print "Total hours %.1fM" % total_hours
-overflow = dict([(key, val['bytes']) for key, val in by_crab_tasks.items() if key >= overflow_bin])
+overflow = dict([(key, val['replica_bytes']) for key, val in by_crab_tasks.items() if key >= overflow_bin])
 #print overflow
 print "Weighted (by bytes) average value of overflow bin: %.1f" % (sum([key*val for key, val in overflow.items()])/float(sum(overflow.values())))
 print "Total accesses by CRAB: %.1fB unique events, %.1fTB read" % (total_unique_events/1e9, total_bytes/1e12)
@@ -109,5 +120,5 @@ print "Current on-disk replicas not accessed: %.1fTB created before this time pe
 print "%.1f" % (unaccessed_old_bytes/1e12)
 print "%.1f" % (unaccessed_bytes/1e12)
 for i in range(1, 15):
-    print '%.1f' % (by_crab_tasks[i]['bytes']/1e12)
+    print '%.1f' % (by_crab_tasks[i]['replica_bytes']/1e12)
 print "%.1f" % tot_bytes
