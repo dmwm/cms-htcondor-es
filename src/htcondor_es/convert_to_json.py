460f853c9576940ee466078449b462726cf28100
Christian Ariza <christian.ariza@gmail.com>
2019-02-11 16:48:05 +0100

    JobLog and PostJobLog URLs
    Fixing format calls, sending values as positional attributes (not dictionary)

--- src/htcondor_es/convert_to_json.py
+++ src/htcondor_es/convert_to_json.py
@@ -747,21 +747,21 @@ def convert_to_json(ad, cms=True, return_dict=False, reduce_data=False):
         result['Processor'] = str(ad['MachineAttrCPUModel0'])
     # Create URL's
     result['jobLogURL'] = 'https://cmsweb.cern.ch/scheddmon/{sched}/{cmsUserId}/{task}/job_out.{job}.{retry}.txt'\
-    .format({
+    .format(
         'sched': result.get('ScheddName','UNKNOWN'),
         'cmsUserId': result['User'],
         'task': result.get('CRAB_Workflow', result['Workflow']),
         'job': result.get('CRAB_Id','0'),
         'retry': result.get('CRAB_Retry','0')
-        })
+        )
     result['postJobLogURL'] = 'https://cmsweb.cern.ch/scheddmon/{sched}/{cmsUserId}/{task}/postjob.{job}.{retry}.txt'\
-    .format({
+    .format(
         'sched': result.get('ScheddName','UNKNOWN'),
         'cmsUserId': result['User'],
         'task': result.get('CRAB_Workflow', result['Workflow']),
         'job': result.get('CRAB_Id','0'),
         'retry': result.get('CRAB_Retry','0')
-        })
+        )
     if reduce_data:
         result = drop_fields_for_running_jobs(result)
 
