34fc3c54ba5d85e5b73b8926ef6fa126608c39a9
Valentin Kuznetsov <vkuznet@gmail.com>
2020-01-17 15:29:12 -0500

    Add filter_attrs to extract attributes from classAds

--- src/htcondor_es/vm.py
+++ src/htcondor_es/vm.py
@@ -4,7 +4,20 @@ import logging
 import requests
 
 
-def post_ads(url, ads, metadata=None):
+def filter_attrs(ads, attrs=None):
+    "Filter attributes from given class ad"
+    doc = {}
+    if attrs:
+        if isinstance(attrs, str):
+            attrs = [attrs]
+        for attr in attrs:
+            if attr in ads:
+                doc[attr] = ads.get(attr)
+        return doc
+    # default mode
+    return doc
+
+def post_ads(url, ads, metadata=None, attrs=None):
     "Post classAds docs into VM url"
     if not len(ads):
         logging.warning("No new documents found")
@@ -12,7 +25,7 @@ def post_ads(url, ads, metadata=None):
 
     starttime = time.time()
     failed = []
-    data = [ad for _, ad in ads]
+    data = [filter_attrs(ad, attrs) for _, ad in ads]
     req = requests.post(url, data)
     if req.status_code != 200:
         failed = data
